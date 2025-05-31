42d114f32c33e7306149b402e3f8a77f48442b21
udeshika123-cyber <udeshikaprabhashini21@gmail.com>
2025-05-31 20:31:06 +0530

    updated django file

--- my_first_project/myFirstApp/urls.py
+++ my_first_project/myFirstApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('myFirstApp/', views.myFirstApp, name='myFirstApp'),
+    path('members/', views.members, name='members'),
+    path('all_members/', views.all_members, name='all_members'),
 ]
